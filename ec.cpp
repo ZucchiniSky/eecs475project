31b81f46dbcd20904487be9185fbc3c106233c22
foodmonkey <jasonphe@umich.edu>
2014-11-30 22:18:16 -0500

    Update ec.cpp

--- ec.cpp
+++ ec.cpp
@@ -184,13 +184,10 @@ mpz_class ECsystem::decrypt(pair<mpz_class, mpz_class> ciphertext){
 
 int main(void){
 	srand(time(0));
-	cout << "Hi" << endl;
 	
 	ECsystem ec;
 	mpz_class incrementVal;	
 	pair <ECpoint, mpz_class> keys = ec.generateKeys();
-	
-	cout << "gen'd keys" << endl;
 	
 	mpz_class plaintext = MESSAGE;
 	ECpoint publicKey = keys.first;
@@ -199,7 +196,6 @@ int main(void){
 	cout<<"Enter offset value for sender's private key"<<endl;
 	cin>>incrementVal;
 	mpz_class privateKey = XB + incrementVal;
-	/*
 	pair<mpz_class, mpz_class> ciphertext = ec.encrypt(publicKey, privateKey, plaintext);	
 	cout<<"Encrypted ciphertext is: ("<<ciphertext.first<<", "<<ciphertext.second<<")\n";
 	mpz_class plaintext1 = ec.decrypt(ciphertext);
@@ -211,8 +207,7 @@ int main(void){
 	if(plaintext == plaintext1)
 		cout << "Correct!" << endl;
 	else
-		cout << "Plaintext different from original plaintext." << endl;	
-		*/	
+		cout << "Plaintext different from original plaintext." << endl;		
 	return 1;
 
 }
