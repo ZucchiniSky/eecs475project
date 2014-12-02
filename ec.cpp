89052fc2ffc20b5e65184b799358e7437e9c53e1
ZucchiniSky <scottbommarito@gmail.com>
2014-12-01 23:18:45 -0500

    how about now

--- ec.cpp
+++ ec.cpp
@@ -107,7 +107,7 @@ ECpoint ECpoint::repeatSum(ECpoint p, mpz_class v) const {
     for (; val > 0; val = val / 2)
     {
     	Q = Q + Q;
-    	if (v % val)
+    	if (v % val > 0)
     	{
     		Q = Q + p;
     	}
@@ -133,7 +133,7 @@ Zp ECsystem::power(Zp val, mpz_class pow) {
     {
         cout << "mod is " << mod << endl;
     	Q = Q * Q;
-    	if (value % mod)
+    	if (value % mod > 0)
     	{
             cout << "yes" << endl;
     		Q = Q * value;
