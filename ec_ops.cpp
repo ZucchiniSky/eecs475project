cfa8da8d4bfa7edef9899d6bda07d6bedc33ab08
foodmonkey <jasonphe@umich.edu>
2014-12-02 00:44:02 -0500

    Update ec_ops.cpp

--- ec_ops.cpp
+++ ec_ops.cpp
@@ -66,7 +66,7 @@ ostream& operator << (ostream& output, const ECpoint& a){
 //================= Methods for ECsystem =================
 
 pair <ECpoint, mpz_class> ECsystem::generateKeys(){
-	Zp test = power(Zp(7), 4);
+	Zp test = power(Zp(49), 5);
 	cout << "test: " << test << endl;
 	//Generate the private key and public key for the user to whom message is sent
 	//Returns only the "P" value of public key and "a" value of private key, 
